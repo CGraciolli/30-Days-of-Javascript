adbbae1bd2b7e42a15f2dbbe6736a1bdba308a2f
CGraciolli <cgraciolli@gmail.com>
2023-01-10 11:37:47 +0100

    day 15

---
+++ day15/index.js
@@ -0,0 +1,46 @@
+const addItems = document.querySelector('.add-items')
+const itemsList = document.querySelector('.plates')
+const items = JSON.parse(localStorage.getItem("items")) || []
+
+function addItem(e){
+    //stops the page from refreshing
+    e.preventDefault()
+    const text = (this.querySelector("[name=item]")).value
+    const item = {
+        text : text, 
+        done : false
+    }
+    items.push(item)
+    populateList(items, itemsList)
+    //we can only use strings in local storage
+    localStorage.setItem("items", JSON.stringify(items))
+    this.reset()
+}
+
+function populateList(plates = [], platesList){
+    platesList.innerHTML = plates.map((plate, i) =>{
+        return `
+        <li>
+            <input type="checkbox" data-index=${i} id="item${i}"
+            ${plate.done ? "checked" : ""}>
+            <label for="item${i}">${plate.text}</label>
+        </li>
+        `
+    }).join("")
+}
+
+function toggleDone(e){
+    if(!e.target.matches("input")) return
+    const elem = e.target
+    const index = elem.dataset.index
+    items[index].done = !items[index].done
+    localStorage.setItem("items", JSON.stringify(items))
+    populateList(items, itemsList)
+}
+
+addItems.addEventListener("submit", addItem)
+itemsList.addEventListener("click", toggleDone)
+
+populateList(items, itemsList)
+
+
