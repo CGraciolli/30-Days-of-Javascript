ebf345b16212358d8bbf2da52b7b7caa37c17098
CGraciolli <cgraciolli@gmail.com>
2023-02-01 14:36:18 +0100

    day 29

--- day 29/index.js
+++ day 29/index.js
@@ -1,9 +1,51 @@
+let countdown 
+const display = document.querySelector(".display__time-left")
+const endtime = document.querySelector(".display__end-time")
+const buttons = document.querySelectorAll(".timer__button")
+
 function timer(seconds){
+    clearInterval(countdown)
     const now = Date.now()
     const then = now + seconds*1000
-    setInterval(() =>{
+    displayTimeLeft(seconds)
+    displayEndTime(then)
+    
+    countdown = setInterval(() =>{
         const secondsLeft = Math.round((then - Date.now())/1000)
-        console.log(secondsLeft)
+        if (secondsLeft < 0){
+            clearInterval(countdown)
+            return
+        }
+        displayTimeLeft(secondsLeft)
     }, 1000)
 
 }
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds/60)
+    seconds = seconds % 60
+    const time = `${minutes}:${seconds > 9 ? "" : "0"}${seconds}`
+    display.textContent = time
+    document.title = time
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp)
+    const hour = end.getHours()
+    const minutes = end.getMinutes()
+
+    endtime.textContent = `Be back at ${hour}:${minutes > 9 ? "" : "0"}${minutes}`
+}
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time)
+    timer(seconds)
+}
+
+buttons.forEach(button => button.addEventListener("click", startTimer))
+
+document.customForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const min = this.minutes.value
+    timer(min*60)
+})
